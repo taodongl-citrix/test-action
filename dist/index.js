386d97208e6150409b86fa60cfdfcb33f5bae862
taodongl-citrix <taodong.lu@citrix.com>
2020-07-01 15:25:24 +0800

    support pull-request

--- dist/index.js
+++ dist/index.js
@@ -1206,93 +1206,108 @@ const exec = __webpack_require__(986);
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
-async function annotationGenerate(accessToken, annotations) {
-    const octokit = new github.getOctokit(accessToken);
-    const req = {
-      ...github.context.repo,
-      ref: "b3f4f26d7f8340f6d3f59cfda712d98f45024712",
-    };
-    console.log(JSON.stringify(req));
-    // if (github.context.payload['pull_request']) {
-    //   req.ref = github.context.payload.pull_request.head.sha
-    // }
-    const res = await octokit.checks.listForRef(req);
-    const jobName = process.env.GITHUB_JOB;
-    console.log("=======================++++================");
-    console.log(JSON.stringify(github.context));
-    console.log("=======================----================");
-    console.log(JSON.stringify(res.data));
-    const checkRun = res.data.check_runs.find(
-      (check) => check.name === jobName
-    );
-    if (!checkRun) {
-      console.log("current job++: " + jobName);
-      console.log(
-        "Can happen when performing a pull request from a forked repository."
-      );
-      return;
+async function generateAnnotation(octokit, json) {
+  let annotations = [];
+  const annotation_level = json.errors > 0 ? "failure" : "notice";
+  const annotation = {
+    path: "g11n-radar",
+    start_line: 0,
+    end_line: 0,
+    start_column: 0,
+    end_column: 0,
+    annotation_level,
+    message: `G11n radar ${json.errors} Errored`,
+  };
+  if (json.errors > 0 || json.warnings > 0) {
+    for (const issue of json.issues) {
+      annotations.push({
+        path: issue.file,
+        start_line: issue.position.startLine,
+        end_line: issue.position.endLine,
+        start_column: issue.position.startColumn,
+        end_column: issue.position.endColumn,
+        annotation_level: "failure",
+        message: issue.context,
+      });
     }
-    const check_run_id = checkRun.id;
-    // const check_run_id = github.context.runId;
+  }
+  annotations = [annotation, ...annotations];
 
-    const update_req = {
-      ...github.context.repo,
-      check_run_id,
-      output: {
-        title: "G11n Radar Results",
-        summary: "G11n Radar Results",
-        annotations,
-      },
-    };
-    await octokit.checks.update(update_req);
+  const req = {
+    ...github.context.repo,
+    ref: github.context.sha,
+  };
+  const res = await octokit.checks.listForRef(req);
+  const jobName = process.env.GITHUB_JOB;
+  const checkRun = res.data.check_runs.find(check => ((check.name === jobName) && (check.status !== 'completed')));
+  if (!checkRun) {
+    console.log(
+      "Can happen when performing a pull request from a forked repository."
+    );
+    return;
+  }
+  const check_run_id = checkRun.id;
+
+  const update_req = {
+    ...github.context.repo,
+    check_run_id,
+    output: {
+      title: "G11n Radar Results",
+      summary: "G11n Radar Results",
+      annotations,
+    },
+  };
+  await octokit.checks.update(update_req);
+}
+
+async function generateReviews(octokit, json) {
+  const pull_number = github.context.payload.pull_request.number;
+  const req = {
+    ...github.context.repo, // owner & repo
+    pull_number, 
+    event: 'COMMENT',
+    body: 'please fix g11n issues',
+  }
+  await octokit.pulls.createReview(req);
+}
+
+async function handleEvent(accessToken, json) {
+  const octokit = new github.getOctokit(accessToken);
+  if (github.context.eventName === 'push') {
+    // add annotations
+    await generateAnnotation(octokit, json)
+  } else if (github.context.eventName === 'pull_request') {
+    // review comments
+    await generateReviews(octokit, json)
+  }
 }
 
 async function run() {
-    try {
-        let annotations = [];
-        const skipList = core.getInput('skip');
-        const accessToken = core.getInput("access-token");
-        const radar = path.resolve(__dirname, '..', 'bin', 'g11n-radar')
-        const project =  process.cwd()
-        const report = path.resolve(project, 'report.json')
-        console.log("parameter: " + skipList)
-        console.log("radar: " + radar)
-        console.log("project: " + project)
-        const skips = skipList.split(',')
-        await exec.exec(radar, ['-p', project, '-d', report, 'rule', '--skip', 'bundlegen/', ...skips]);
-        const data = await fs.promises.readFile(report);
-        var json = JSON.parse(data);
-        const annotation_level = json.errors > 0 ? "failure" : "notice";
-        const annotation = {
-            path: "g11n-radar",
-            start_line: 0,
-            end_line: 0,
-            start_column: 0,
-            end_column: 0,
-            annotation_level,
-            message: `G11n radar ${json.errors} Errored`,
-        };
-        if (json.errors > 0 || json.warnings > 0) {
-            for (const issue of json.issues) {
-                annotations.push({
-                    path: issue.file,
-                    start_line: issue.position.startLine,
-                    end_line: issue.position.endLine,
-                    start_column: issue.position.startColumn,
-                    end_column: issue.position.endColumn,
-                    annotation_level: "failure",
-                    message: issue.context,
-                });
-            }
-        }
-        annotations = [annotation, ...annotations];
-        await annotationGenerate(accessToken, annotations)
-    } catch (error) {
-        core.setFailed(error.message);
+  try {
+    const skipList = core.getInput('skip');
+    const accessToken = core.getInput("access-token");
+    const radar = path.resolve(__dirname, '..', 'bin', 'g11n-radar')
+    const project =  process.cwd()
+    const report = path.resolve(project, 'report.json')
+    console.log("parameter: " + skipList)
+    console.log("radar: " + radar)
+    console.log("project: " + project)
+    const skips = skipList.split(',')
+    await exec.exec(radar, ['-p', project, '-d', report, 'rule', '--skip', 'bundlegen/', ...skips]);
+    const data = await fs.promises.readFile(report);
+    var json = JSON.parse(data);
+    if (json.errors == 0) {
+      return;
+    } else {
+      await handleEvent(accessToken, json);
+      core.setFailed('g11n issues exist');
     }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-run()
+run();
 
 /***/ }),
 
